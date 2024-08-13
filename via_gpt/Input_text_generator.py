26da6fefc94973902dc8bf5a063f6c20d12cfb57
gggg8657 <gggg8657@gmail.com>
2024-08-13 14:22:27 +0900

    new query

--- via_gpt/Input_text_generator.py
+++ via_gpt/Input_text_generator.py
@@ -11,6 +11,16 @@ def json_to_query(file):
 
         query = f"사용자의 대변 색상 코드: {color_code}, 브리스톨 지수: {bristol}, 혈액 존재: {'있음' if blood else '없음'}이야, 이에 대한 건강 상태와 관련된 의심되는 상황을 해결하기 위해, 나에게 추가적인 질문을 한가지만 해줘"
         
+        response = client.chat.completions.create(
+            model="gpt-4o"
+            response_format={"type": "json_object"},
+            messages=[
+                {"role":"system",
+                "content": "You are a helpful assistant designed to ouput JSON."},
+                {"role": "user", "content": f"{query}"},
+            ]
+        )
+
         return query
 
 
