4aacc00ffacffced068991950c1346393a2d8233
gggg8657 <gggg8657@gmail.com>
2024-08-13 16:32:21 +0900

    mute all

--- via_gpt/poopt.py
+++ via_gpt/poopt.py
@@ -1,8 +1,9 @@
 from Input_text_generator import json_to_query
 
 def query_to_gpt():
-    query = json_to_query('/mnt/sdb/tmp/poopsee/langchain/via_gpt/dummy.json')
+    query,response = json_to_query('/mnt/sdb/tmp/poopsee/langchain/via_gpt/dummy.json')
     print(query)
-
+    print(f"response json string :: {response}")
+    
 if __name__ == "__main__":
     query_to_gpt()
